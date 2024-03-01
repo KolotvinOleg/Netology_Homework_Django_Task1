b56bffa80e8ecc3a1eab407537977999a7927b7a
Kolotvin_Oleg <Kolotvin.Oleg@gmail.com>
2024-03-01 12:49:22 +0400

    CRUD commit

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Product, Stock, StockProduct
+
 
 # Register your models here.
+
+class StockInline(admin.TabularInline):
+    model = StockProduct
+    extra = 0
+    verbose_name = 'Продукт'
+    verbose_name_plural = 'Продукты'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_editable = ['description']
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['address']
+    inlines = [StockInline, ]
